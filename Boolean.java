5a4cabe2f1c5108860aeb74267277a725961c626
manishbeth18 <manish.beth@gmail.com>
2025-09-08 22:09:06 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,17 @@
+public class Boolean {
+    public static void main(String[] args){
+        int a=10;
+        int b=12;
+        int result=a&b;
+        int result1=a|b;
+        int result2=a^b;
+        int result3=~b;
+        System.out.println("a&b="+result);
+        System.out.println("a|b="+result1);
+        System.out.println("a^b="+result2);
+        System.out.println("~b="+result3);
+        
+
+    }
+
+}
